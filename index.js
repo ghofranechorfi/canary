9c871e44191eea4a7b0b4d239477c617ca6dbcdd
Ghofrane Chorfi <ghofranechorfi123@gmail.com>
2021-11-25 09:32:49 +0100

    connexion mysql

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 const path = require('path');
 
 app.use(express.static('public'));
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "canary"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+}); 
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/views/index.html'));
@@ -16,6 +30,15 @@ app.get('/signin', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/signup.html'));
+})
+
+app.get('/profile/:username', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/profile.html'));
+    console.log(req.params.username);
+    con.query("SELECT * FROM user where username = ?" , req.params.username, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+      });
 })
 
 
